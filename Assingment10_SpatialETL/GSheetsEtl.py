2a8afe622acdebdc190b95cf091d7387f9389094
TwistedCodes <100046346+TwistedCodes@users.noreply.github.com>
2022-04-02 19:19:41 -0600

    Assignment 10
    Here are the files for assignment 10

---
+++ Assingment10_SpatialETL/GSheetsEtl.py
@@ -0,0 +1,10 @@
+from SpatialEtl import SpatialETL
+
+class GSheetsEtl(SpatialETL):
+    def __init__(self, remote, local_dir, data_format, destination):
+            super().__init__(remote, local_dir, data_format, destination)
+
+    def process(self):
+        super().extract()
+        super().transform()
+        super().load()
