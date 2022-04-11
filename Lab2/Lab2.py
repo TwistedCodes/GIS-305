e95848700db195bbfa369c59c24d0278bd92d0b1
TwistedCodes <100046346+TwistedCodes@users.noreply.github.com>
2022-04-11 16:18:30 -0600

    Add files via upload

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -9,14 +9,16 @@ def setup():
     with open('config/wnvoutbreak.yaml') as f:
         config_dict = yaml.load(f, Loader=yaml.FullLoader)
 
-    arcpy.env.workspace = r"C:\Users\Owner\Documents\305 Python\Lab 1A\WestNileOutbreak\WestNileOutbreak.gdb"
+    arcpy.env.workspace = f"{config_dict('proj_dir')}WestNileOutbreak.gdb"
     arcpy.env.overwriteOutput = True
     return config_dict
+
 
 def etl():
     print("etling....")
     etl_instance = GSheetsEtl(config_dict)
     etl_instance.process()
+
 
 def buffer(layer_name, buf_dist):
     # Buffer the incoming layer by the buffer distance
@@ -41,6 +43,9 @@ def spatial_join():
     arcpy.analysis.SpatialJoin("BoulderAddresses", "Intersect", "Areas_of_concern")
 
 
+# def symmetrical difference():
+# arcpy.analysis.SymDiff(Avoid_Points, update_features, Avoid_Area, {ALL}, {0.15 mile})
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     global config_dict
@@ -52,11 +57,13 @@ if __name__ == '__main__':
     # # wetlands
     # # lakes and reservoirs
     # # open space (OSMP)
-    # buffer_layer_list = ["Mosquito_Larval_Sitess", "LakesandRes_Boulder", "OSMP_properties", "Wetlands_Boulder"]
-    # for layer in buffer_layer_list:
-    #     print("Looping")
-    #     buffer(layer, "0.1 mile")
-    # int_lyrs = ["LakesandRes_Boulder_buf", "Mosquito_Larval_Sitess_buf", "OSMP_properties_buf", "Wetlands_Boulder_buf"]
-    # intersect("Intersect", int_lyrs)
-    #
-    # spatial_join()
+    # Avoid_Points
+    buffer_layer_list = ["Mosquito_Larval_Sitess", "LakesandRes_Boulder", "OSMP_properties", "Wetlands_Boulder"]
+    for layer in buffer_layer_list:
+        print("Looping")
+        buffer(layer, "0.1 mile")
+        int_lyrs = ["LakesandRes_Boulder_buf", "Mosquito_Larval_Sitess_buf", "OSMP_properties_buf",
+                    "Wetlands_Boulder_buf"]
+    intersect("Intersect", int_lyrs)
+
+    spatial_join()
--- Lab2/main.py
+++ Lab2/main.py
@@ -6,9 +6,13 @@ import json
 
 def extract():
     print("Extracting addresses from google form spreadsheet")
-    #file = urllib.request.urlopen("https://docs.google.com/spreadsheets/d/e/2PACX-1vTaJ_1xRhGQAOSITkgn_C1wfPSnPX0BA37XuftlXVfVrpjfj4J3BHPu1soGeUtNt3XjLI1G_HT2Fy69/pub?output=csv")
+    # file = urllib.request.urlopen(
+    # "https://docs.google.com/spreadsheets/d/e/2PACX
+    # -1vTaJ_1xRhGQAOSITkgn_C1wfPSnPX0BA37XuftlXVfVrpjfj4J3BHPu1soGeUtNt3XjLI1G_HT2Fy69/pub?output=csv")
 
-    r = requests.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vTaJ_1xRhGQAOSITkgn_C1wfPSnPX0BA37XuftlXVfVrpjfj4J3BHPu1soGeUtNt3XjLI1G_HT2Fy69/pub?output=csv")
+    r = requests.get("https://docs.google.com/spreadsheets/d/e/2PACX"
+                     "-1vTaJ_1xRhGQAOSITkgn_C1wfPSnPX0BA37XuftlXVfVrpjfj4J3BHPu1soGeUtNt3XjLI1G_HT2Fy69/pub?output"
+                     "=csv")
     r.encoding = "utf-8"
     data = r.text
     with open(r"C:\Users\Owner\Downloads\addresses.csv", "w") as output_file:
@@ -25,7 +29,7 @@ def transform():
         for row in csv_dict:
             address = row["Street Address"] + " Boulder CO"
             print(address)
-            geocode_url = "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=" + address + "&benchmark=2020&format=json"
+            geocode_url = "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=" + address + "&benchmark=2020&format=json "
             print(geocode_url)
             r = requests.get(geocode_url)
 
@@ -50,12 +54,12 @@ def load():
     x_coords = "X"
     y_coords = "Y"
 
-
     # Make the XY event layer...
     arcpy.management.XYTableToPoint(in_table, out_feature_class, x_coords, y_coords)
 
     # Print the total rows
     print(arcpy.GetCount_management(out_feature_class))
+
 
 if __name__ == "__main__":
     extract()
