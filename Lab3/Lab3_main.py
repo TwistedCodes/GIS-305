97bd3d207bd484ed6c029ad4cbca6b0b4bb21712
TwistedCodes <100046346+TwistedCodes@users.noreply.github.com>
2022-04-24 18:36:27 -0600

    Add files via upload

--- Lab3/Lab3_main.py
+++ Lab3/Lab3_main.py
@@ -3,8 +3,8 @@ import arcpy
 from GSheetsEtl import GSheetsEtl
 import logging
 
-
 logging.info('Starting West Nile Virus Script')
+
 
 def setup():
     with open('config/wnvoutbreak.yaml') as f:
@@ -34,17 +34,18 @@ def buffer(layer_name, buf_dist):
 def intersect(layer_name, int_lyrs):
     # Use a breakpoint in the code line below to debug your script.
     logging.debug("Starting Intersect")
-    print(f'My intersect Method')
 
     output_intersect_name = "Intersect"
     arcpy.analysis.Intersect(int_lyrs, output_intersect_name)
     logging.debug("End Intersect Fuction")
+
 
 def spatial_join():
     logging.debug("Starting spatial join")
     # Use a breakpoint in the code line below to debug your script.
     arcpy.analysis.SpatialJoin("BoulderAddresses", "Intersect", "Areas_of_concern")
     logging.debug("My spatial join Method")
+
 
 def erase():
     logging.debug("Begin erase")
@@ -53,12 +54,14 @@ def erase():
 
 
 def exportMap():
-    aprx = arcpy.mp.ArcGISProject(f({config_dict.get('proj_dir')}'WestNileOutbreak.aprx'))
+    aprx = arcpy.mp.ArcGISProject = f({config_dict.get('proj_dir')}, 'WestNileOutbreak.aprx'),
     lyt = aprx.listLayouts()[0]
     for el in lyt.listElements():
-	print(el.name)
-	if "Title" in el.name:
-	    el.text = el.text + "custom message"
+        print(el.name)
+    if "Title" in el.name:
+        el.text = el.text + "Map Export Complete"
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     global config_dict
@@ -75,7 +78,7 @@ if __name__ == '__main__':
     # Avoid_Points
     buffer_layer_list = ["Mosquito_Larval_Sitess", "LakesandRes_Boulder", "OSMP_properties", "Wetlands_Boulder"]
     for layer in buffer_layer_list:
-        print("Looping")
+        logging.debug("Looping")
         buffer(layer, "0.15 mile")
         int_lyrs = ["LakesandRes_Boulder_buf", "Mosquito_Larval_Sitess_buf", "OSMP_properties_buf",
                     "Wetlands_Boulder_buf"]
